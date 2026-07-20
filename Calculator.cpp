6e92a5441d39cdfa5e32237c2cc92997464da6cc
Jackson <jackson.manly47@gmail.com>
2026-07-20 18:16:04 +1000

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,7 +1,8 @@
 // Calculator.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream> <cmath>
+#include <iostream> 
+#include <cmath>
 void showmenu()
 {
     std::cout << "Choose an operation:\n";
@@ -29,9 +30,9 @@ double multiply(double x, double y)
 
 double division(double x, double y)
 {
-    if (y ==0)
-    {       
-        std::cout << "Cannot divide by zero";
+    if (y == 0)
+    {
+        std::cout << "Cannot divide by zero!";
         return 0;
     }
     else
@@ -40,43 +41,60 @@ double division(double x, double y)
 
 double squared(double x, double y)
 {
-   return pow(x,y);
+    return pow(x, y);
 }
 
     int main()
 {
-    double x{};
-    double y{};
-    int choice{};
-    
-    std::cout << "Select your first number:\n";
-    std::cin >> x;
-    
-    showmenu();
-        std::cin >> choice;
+        char again{'Y'};
+        while (again == 'Y' || again == 'y')
+        {
+            double x{};
+            double y{};
+            int choice{};
+
+            std::cout << "Select your first number:\n";
+            std::cin >> x;
+
+            showmenu();
+            std::cin >> choice;
+
+            std::cout << "Enter your 2nd number:\n";
+            std::cin >> y;
+
+            if (choice == 1)
+
+                std::cout << "Answer:" << add(x, y);
+
+            else if (choice == 2)
+
+                std::cout << "Answer:" << subtract(x, y);
+
+            else if (choice == 3)
+                std::cout << "Answer:" << multiply(x, y);
+
+            else if (choice == 4)
+                std::cout << "Answer:" << division(x, y);
+            else if (choice == 5)
+                std::cout << "Answer:" << squared(x, y);
+            else
+                std::cout << "Invalid";
+            std::cout << "\nDo another calculation? (Y/N)";
+            std::cin >> again;
+            if (again != 'Y' && again != 'y' && again != 'N' && again != 'n')
+            {
+                std::cout << "Invalid input.\n";
+        }
+
+     
+        
+        std::cout << "Thank you for using my calculator!";
+       
       
-      std::cout << "Enter your 2nd number:\n";
-      std::cin >> y;
-
-      if (choice == 1)
-
-          std::cout << "Answer:" << add(x, y);
-
-      else if (choice == 2)
-
-          std::cout << "Answer:" << subtract(x, y);
-
-      else if (choice == 3)
-          std::cout << "Answer:" << multiply(x, y);
-
-      else if (choice == 4)
-          std::cout << "Answer:" << division(x, y);
-      else if (choice == 5)
-          std::cout << "Answer:" << squared(x,y);
-      else
-          std::cout << "Invalid";
-
-      return 0;
+  
+        return 0;
+    
+    }
 
 
 
